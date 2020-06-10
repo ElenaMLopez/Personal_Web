9b016db28a2e0717f09e09a0d73b84e9cde07ec0
elenamlopez <elena.mateos1@gmail.com>
2020-06-10 14:52:49 +0200

    Add some info

--- assets/js/own.js
+++ assets/js/own.js
@@ -1,7 +1,13 @@
 
 var showInfoBtn = document.getElementById('showInfo');
 var extraInfo = document.getElementById('extraInfo');
+var showInfoBtn2 = document.getElementById('showInfo2');
+var extraInfo2 = document.getElementById('extraInfo2');
 
 showInfoBtn.addEventListener('click', function(e){
   extraInfo.classList.toggle('hidden');
+})
+
+showInfoBtn2.addEventListener('click', function(e){
+  extraInfo2.classList.toggle('hidden');
 })
